d106f020e58825c910f42ac82f7700958980f499
David Bustillos. <136139565+DavidJBM@users.noreply.github.com>
2024-11-24 16:08:35 -0600

    Deje bonito el login
    si

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_db_connection():
-    return mysql.connector.connect(user="root", password="", host="localhost", port="3308", database="myclassmate")
+    return mysql.connector.connect(user="root", password="", host="localhost", port="3306", database="myclassmate")
 
 
 @app.route('/')
